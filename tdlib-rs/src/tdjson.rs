ef170492f0b6cf35ab6b72fb7d622a85cdca9554
jurij-jukic <j.jjukicc@gmail.com>
2024-06-06 19:41:15 +0200

    try to run on wasm32-wasi

--- tdlib-rs/src/tdjson.rs
+++ tdlib-rs/src/tdjson.rs
@@ -10,7 +10,7 @@
 use std::ffi::{CStr, CString};
 use std::os::raw::{c_char, c_double, c_int};
 
-#[link(name = "tdjson")]
+#[link(name = "tdjson_static")]
 extern "C" {
     fn td_create_client_id() -> c_int;
     fn td_send(client_id: c_int, request: *const c_char);
