f0ea644f6026432127c3a0c1ffabf810c0f68314
Andrea Longoni <57501821+Andreal2000@users.noreply.github.com>
2025-04-13 11:57:26 +0200

    Add Linux ARM support

--- tdlib-rs/build.rs
+++ tdlib-rs/build.rs
@@ -73,6 +73,7 @@ fn copy_local_tdlib() {
 /// Build the project using the generic build configuration.
 /// The current supported platforms are:
 /// - Linux x86_64
+/// - Linux aarch64
 /// - Windows x86_64
 /// - MacOS x86_64
 /// - MacOS aarch64
@@ -82,7 +83,10 @@ fn generic_build() {
     let include_dir = format!("{}/include", prefix);
     let lib_dir = format!("{}/lib", prefix);
     let lib_path = {
-        #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
+        #[cfg(any(
+            all(target_os = "linux", target_arch = "x86_64"),
+            all(target_os = "linux", target_arch = "aarch64")
+        ))]
         {
             format!("{}/libtdjson.so.{}", lib_dir, TDLIB_VERSION)
         }
